37a329176a31040cb653ac38aa6fa8fa20eb72de
IrinduN <n.irindu@mubasher.net>
2020-05-23 12:46:43 +0530

    Added date printing

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,13 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        Date date = new Date();
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        System.out.println(simpleDateFormat.format(date));
     }
 }
